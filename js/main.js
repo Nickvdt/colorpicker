a2771ccc3ff95335d6cf817516db80cb99ec05dc
Nickvdt <32934@ma-web.nl>
2023-02-22 15:35:39 +0100

    Colorpicker afgemaakt

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 const colors = document.getElementsByClassName("colors__color");
 let secondColor = false;
 for(let i = 0; i < colors.length; i++){
-    /* set color */
-    if(secondColor === false){
-        colors[i].children[0].style.background = "#6096B4"
-        secondColor = true;
-    }
-    else{
-        colors[i].children[0].style.background = "#F48484"
-        secondColor = false;
-    }
-
-    /* onlick */
+    // nummer inclusief 1 - 360 inclusief
+    // percentage inclusief 11 - 79 => saturatie
+    // percentage inclusief 11 - 100 => licht
+    //x = Max.random() * (max - min) + min;
+    let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
+    let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
+    let randomLightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+    
+    colors[i].children[0].style.background = `hsl(${randomHue} ${randomSaturation} ${randomLightness})`;
+    // onlick 
     colors[i].onclick = function(){
         colors[i].children[0].classList.add("colors__circle--selected");
         navigator.clipboard.writeText(colors[i].children[0].style.background);
